30d7c68d4424fe8808b4688759fa27587621bc2e
matthewpeters <matthew.peters@ufl.edu>
2013-08-13 15:42:56 -0400

    MapEdit	Work
    -added popup box for reset alert
    -added reset all alert
    -added reset localization support
    -added warn on leaving page
    -added filter to leaving page for debug
    -added some xml comments
    -changed to standard tab format
    -added rib mode support
    -added action placers when changing actions

--- SobekCM/default/scripts/serverside/Scripts.aspx.cs
+++ SobekCM/default/scripts/serverside/Scripts.aspx.cs
@@ -10,10 +10,10 @@ using SobekCM.Resource_Object.Metadata_Modules.GeoSpatial;
 public partial class default_scripts_serverside_Scripts : System.Web.UI.Page
 {
 
-    [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
-    public static void SaveItem(String sendData)
-    {
-        SobekCM.Library.ItemViewer.Viewers.Google_Coordinate_Entry_ItemViewer.SaveContent(sendData);
-    }
+  [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+  public static void SaveItem(String sendData)
+  {
+    SobekCM.Library.ItemViewer.Viewers.Google_Coordinate_Entry_ItemViewer.SaveContent(sendData);
+  }
 
 }
