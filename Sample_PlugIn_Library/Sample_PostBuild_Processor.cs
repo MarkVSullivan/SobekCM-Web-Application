ea9eafd92ad84a9a17f1e0426096cef134e4098c
Keven1894 <keven1894@gmail.com>
2013-09-06 16:26:52 -0400

    Demonstration of using sample plug-in library and beginning to add the hooks for custom actions within the builder.

---
+++ Sample_PlugIn_Library/Sample_PostBuild_Processor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample_PlugIn_Library
+{
+    class Sample_PostBuild_Processor : SobekCM.Library.Builder.iBuilder_PostBuild_Process
+    {
+        public void PostProcess(SobekCM.Resource_Object.SobekCM_Item Built_Object, string Directory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
