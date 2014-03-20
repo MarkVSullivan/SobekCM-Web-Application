8cef361ce5d04fabc16da5d8847ed526614c4058
Suchitra Yellapantula <y.suchitra@gmail.com>
2014-03-20 18:50:12 -0400

    Homepage aggregations- hover details
    - Added on-hover expanded detail for the aggregation icons
    - Replaced JQuery QTip with JQuery Hovercard
    - Fixed a couple of z-index related bugs, other CSS issues in hovercard.js

--- SobekCM/default/scripts/sobekcm_aggre.js
+++ SobekCM/default/scripts/sobekcm_aggre.js
@@ -1,62 +1,79 @@
-$(document).ready(function() {
+//$(document).ready(function() {
     
-    //$('#sbkAghsw_CollectionButtonImg0').qtip(
-    $('[id*=sbkAghsw_CollectionButtonImg').each(function() {
-        var $this = $(this);
+//    $('[id*=sbkAghsw_CollectionButtonImg').each(function() {
+//        var $this = $(this);
 
 
-     // var divToDisplay = '<div id="leftDivHover">' + image+'</div>'
-        $this.addClass('sbk_hoverTooltipStyle');
-        $this.qtip(
-            {
-                overwrite: false,
-                content: {
-                    //Get the title from the nested img element's alt attribute
-                  title: $this.find('img').attr('alt'),
-                  text: $this.attr('title')
+//     // var divToDisplay = '<div id="leftDivHover">' + image+'</div>'
+//        $this.addClass('sbk_hoverTooltipStyle');
+//        $this.qtip(
+//            {
+//                overwrite: false,
+//                content: {
+//                    //Get the title from the nested img element's alt attribute
+//                    title: $this.find('img').attr('alt'),
+//                    text: '<div><div style=\"display:inline; float:left; margin-right: 5px;\"><img src="'+$(this).find('img').attr('src')+'"/></div><div style=\"display:inline;\">'+$this.attr('title')+'</div></div>'
+    
 
-                },
+//                },
       
-                style: {
-                    classes: 'ui-tooltip-dark ui-tooltip-shadow',
-                //    classes: { tooltip: 'sbk_hoverTooltipStyle' },
-                    tip: {
-                        corner: 'left center',
-                        border: 1,
-                        width: 22,
-                        height: 11
-                    }, // Give it a speech bubble tip
+//                style: {
+                    
+//                    title:{'color':'red'},
+//                    name : 'Bootstrap',
+//                //    classes: { tooltip: 'sbk_hoverTooltipStyle' },
+//                    tip: {
+//                        corner: 'left center',
+//                        border: 1,
+//                        width: 22,
+//                        height: 11
+//                    }, // Give it a speech bubble tip
                    
-                },
+//                },
              
-             
+//                border:{color:'red'},
                 
-                position: {
-                   my: "left center",
-                   at: "right center",
-                    viewport: $(window)
-                },
-                show: {
-                    delay:0,
-                    mouseover: true,
-                    solo:true
+//                position: {
+//                   my: "left center",
+//                   at: "right center",
+//                    viewport: $(window)
+//                },
+//                show: {
+//                    delay:0,
+//                    mouseover: true,
+//                    solo:true
                             
-                    },
-                hide: {
-                    fixed:true
-                }
+//                    },
+//                hide: {
+//                    fixed:true
+//                }
      
                     
                 
-            });
-    });
-});
-
-//$(document).ready(function () {
-//    var testHTML = '<p>This is a test description</p>';
-//    alert(testHTML);
-//    $('#sbkAghsw_CollectionButtonImg0').hovercard({
-//        detailsHTML: testHTML,
-//        width: 400
+//            });
 //    });
 //});
+
+$(document).ready(function () {
+    
+    
+    $('[id*=sbkAghsw_CollectionButtonImg]').each(function () {
+        var $this = $(this);
+        var hovercardTitle = '<div style=\"display:inline; float:left; font-weight:bold;margin-left:70px;margin-top:-10px;\"><a href=' + $this.find('img').attr('href')+'>' + $this.find('img').attr('alt') + '</a></div><br/>';
+        //   var hovercardHTML = '<div style=\"display:inline;margin:70px;\">' + $this.find('.spanHoverText').text() + '</div><br/><div ><a href='+$this.find('a').attr('href')+'>Go to the collection</a></div>';
+        var hovercardHTML = '<div style=\"display:inline;margin:70px;\">' + $this.find('.spanHoverText').text() + '</div><br/>';
+        
+
+        $this.hovercard({
+            detailsHTML: hovercardTitle+hovercardHTML,
+            width: 300,
+            openOnLeft: false,
+            autoAdjust: false,
+            delay:10
+            
+ 
+        });
+       
+
+    });
+});
