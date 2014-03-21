f038ac25acbbf30748785029eee1cf6e3e74478a
Suchitra Yellapantula <y.suchitra@gmail.com>
2014-03-21 14:36:30 -0400

    Aggregations itemviewer - hovercard changes

--- SobekCM/default/scripts/sobekcm_aggre.js
+++ SobekCM/default/scripts/sobekcm_aggre.js
@@ -57,9 +57,11 @@
 $(document).ready(function () {
     
     
-    $('[id*=sbkAghsw_CollectionButtonImg]').each(function () {
-        var $this = $(this);
-        var hovercardTitle = '<div style=\"display:inline; float:left; font-weight:bold;margin-left:70px;margin-top:-10px;\"><a href=' + $this.find('img').attr('href')+'>' + $this.find('img').attr('alt') + '</a></div><br/>';
+      $('[id*=sbkAghsw_CollectionButtonImg]').each(function () {
+    
+          var $this = $(this);
+          var cssClass = 'sbkAghsw_CollectionButtonTxt';
+        var hovercardTitle = '<div style=\"display:inline; float:left; font-weight:bold;margin-left:70px;margin-top:-10px;\" class=\"'+cssClass+'\"><a href=' + $this.find('img').attr('href')+'>' + $this.find('img').attr('alt') + '</a></div><br/>';
         //   var hovercardHTML = '<div style=\"display:inline;margin:70px;\">' + $this.find('.spanHoverText').text() + '</div><br/><div ><a href='+$this.find('a').attr('href')+'>Go to the collection</a></div>';
         var hovercardHTML = '<div style=\"display:inline;margin:70px;\">' + $this.find('.spanHoverText').text() + '</div><br/>';
         
@@ -69,7 +71,7 @@ $(document).ready(function () {
             width: 300,
             openOnLeft: false,
             autoAdjust: false,
-            delay:10
+            delay:0
             
  
         });
