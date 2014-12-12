cf0b3ab7e430d86f2f25cb10bd6cc0295a0df48e
MarkVSullivan <Mark.V.Sullivan@gmail.com>
2014-12-12 07:54:23 -0500

    Correct issue with the builder aborting validation prematurely on singe-instance configs

--- SobekCM_Builder_Library/Modules/Folders/ValidateAndClassifyModule.cs
+++ SobekCM_Builder_Library/Modules/Folders/ValidateAndClassifyModule.cs
@@ -26,8 +26,12 @@ namespace SobekCM.Builder_Library.Modules.Folders
                 OnProcess("ValidateAndClassifyModule.Perform_BulkLoader: Begin validating and classifying packages in incoming/process folders", "Verbose", String.Empty, String.Empty, -1);
 
             // If the maximum number of (incoming, non-delete) packages have already been set aside to process, no need to continue on this folder
-            if (IncomingPackages.Count >= Builder_Settings.Instance_Package_Limit)
+            if ((Builder_Settings.Instance_Package_Limit > 0) && (IncomingPackages.Count >= Builder_Settings.Instance_Package_Limit))
+            {
+                OnProcess("...Package validation aborted - maximum number of packages ( "  + Builder_Settings.Instance_Package_Limit + " ) reached", "Verbose", String.Empty, String.Empty, -1);
                 return;
+            }
+                
 
             try
             {
@@ -187,8 +191,11 @@ namespace SobekCM.Builder_Library.Modules.Folders
                             }
 
                             // If the maximum number of (incoming, non-delete) packages has now been met, no need to classify anymore
-                            if (IncomingPackages.Count >= Builder_Settings.Instance_Package_Limit)
+                            if ((Builder_Settings.Instance_Package_Limit > 0) && (IncomingPackages.Count >= Builder_Settings.Instance_Package_Limit))
+                            {
+                                OnProcess("...Package validation aborted - maximum number of packages ( " + Builder_Settings.Instance_Package_Limit + " ) reached", "Verbose", String.Empty, String.Empty, -1);
                                 return;
+                            }
                         }
                     }
                 }
