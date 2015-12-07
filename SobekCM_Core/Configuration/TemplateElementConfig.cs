13ee3c3a79be2d88e12616412723bf0e1651a27a
MarkVSullivan <Mark.V.Sullivan@gmail.com>
2015-12-06 19:46:58 -0500

    Add new config file for mapping template config files to element objects

---
+++ SobekCM_Core/Configuration/TemplateElementConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class TemplateElementConfig
+    {
+        public string Type { get; set; }
+
+        public string SubType { get; set; }
+
+        public string Class { get; set; }
+
+        public string Assembly { get; set; }
+
+
+    }
+}
